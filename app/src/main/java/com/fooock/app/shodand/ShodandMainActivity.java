369851a395bc2e2f8246ff5cc8e1d5e191021237
newhouse <nhitbh@gmail.com>
2017-02-15 22:14:39 +0100

    Added butterKnife dependency

--- app/src/main/java/com/fooock/app/shodand/ShodandMainActivity.java
+++ app/src/main/java/com/fooock/app/shodand/ShodandMainActivity.java
@@ -3,6 +3,9 @@ package com.fooock.app.shodand;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.ButterKnife;
+import timber.log.Timber;
+
 /**
  *
  */
@@ -12,5 +15,8 @@ public class ShodandMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shodand_main);
+        ButterKnife.bind(this);
+
+        Timber.d("In onCreate()");
     }
 }
