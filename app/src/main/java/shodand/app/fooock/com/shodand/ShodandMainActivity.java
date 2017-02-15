467a1e11fe4ed9b73ff47b3365b6e951559daefd
newhouse <nhitbh@gmail.com>
2017-02-15 19:43:24 +0100

    Initial commit

---
+++ app/src/main/java/shodand/app/fooock/com/shodand/ShodandMainActivity.java
@@ -0,0 +1,13 @@
+package shodand.app.fooock.com.shodand;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ShodandMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shodand_main);
+    }
+}
